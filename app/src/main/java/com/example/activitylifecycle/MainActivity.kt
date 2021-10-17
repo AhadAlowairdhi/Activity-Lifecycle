699da31b2065cf9e67e4eb96b2f28e4b368953d5
Ahad <odisha2226@gmail.com>
2021-10-17 08:22:37 +0300

    Activity Lifecycle

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.activitylifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //onStart(), onResume(), onPause(), onStop(), and onDestroy().
+    }
+    override fun onStart() {
+        super.onStart()
+        println("OnStart Method")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart Method")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("onPause Method")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("onStop Method")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("onResume Method")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy Method")
+    }
+}
